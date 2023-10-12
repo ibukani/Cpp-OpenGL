28051f692e175a02712367b1ff453c7142e4478d
ibukani <ibukani2056@gmail.com>
2023-10-12 15:28:05 +0900

    add stb library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
 #include "shaderClass.h"
 #include "VBO.h"
@@ -10,20 +11,17 @@
 // 頂点のデータ
 GLfloat vertices[] =
 {
-	-0.5f,  -0.5f * float(sqrt(3)) / 3,     0.0f, 0.8f, 0.3f,  0.02f,  // 左の角
-	 0.5f,  -0.5f * float(sqrt(3)) / 3,     0.0f, 0.8f, 0.3f,  0.02f,  // 右の角
-	 0.0f,   0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 0.6f,  0.32f,  // 一番上
-	-0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f, 0.17f,  // 真ん中の左
-	 0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f, 0.17f,  // 真ん中の右
-	 0.0f,  -0.5f * float(sqrt(3)) / 3,     0.0f, 0.8f, 0.3f,  0.02f   // 真ん中の下
+	-0.5f,  -0.5f, 0.0f,   1.0f, 0.0f,  0.0f,  // 左の角
+	-0.5f,   0.5f, 0.0f,   0.0f, 1.0f,  0.0f,  // 右の角
+	 0.5f,   0.5f, 0.0f,   0.0f, 0.0f,  1.0f,  // 一番上
+	 0.5f,  -0.5f, 0.0f,   1.0f, 1.0f,  1.0f,  // 真ん中の左
 };
 
 // インデックスのデータ
 GLuint indices[] =
 {
-	0, 3, 5,
-	3, 2, 4,
-	5, 4, 1,
+	0, 2, 1,
+	0, 3, 2,
 };
 
 int main(void)
